164d46d6741246a0ac72efcf20ef3bb4cb478f77
VitorSouza <vsalves0707@gmail.comm>
2025-09-01 16:16:09 -0300

    Finalizado

--- app/src/main/java/com/senai/calculador_de_gorjetas/MainActivity.kt
+++ app/src/main/java/com/senai/calculador_de_gorjetas/MainActivity.kt
@@ -1,20 +1,28 @@
 package com.senai.calculador_de_gorjetas
 
+import android.graphics.drawable.Icon
 import android.icu.text.NumberFormat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -24,6 +32,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,7 +47,9 @@ class MainActivity : ComponentActivity() {
             Calculador_de_GorjetasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                    CalculadorApp(
-                       modifier = Modifier.padding(innerPadding)
+                       modifier = Modifier
+                           .padding(innerPadding)
+                           .fillMaxSize()
                    )
                 }
             }
@@ -47,19 +58,28 @@ class MainActivity : ComponentActivity() {
 }
 @Preview(showSystemUi = true)
 @Composable
-fun CalculadorApp(modifier: Modifier = Modifier
+fun CalculadorApp(
+    modifier: Modifier = Modifier
     .fillMaxSize()
     .wrapContentSize(Alignment.Center),
 ){
     var valorContaTexto by remember { mutableStateOf("") }
-    val valorContaDouble = valorContaTexto.toDoubleOrNull() ?: 0.0
-    val gorjeta = calcularGorjeta(valorContaDouble)
+    var porcentagemGorjeta by remember { mutableStateOf("") }
+    var estadoBotao by remember { mutableStateOf(false) }
+
+    val gorjeta = calcularGorjeta(
+        valorContaTexto.toDoubleOrNull() ?: 0.0,
+        porcentagemGorjeta.toDoubleOrNull() ?: 15.0,
+        estadoBotao
+    )
 
     Column (
         modifier = modifier
             .fillMaxWidth()
-            .padding(horizontal = 50.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+            .padding(horizontal = 50.dp)
+            .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ){
         Text(
             stringResource(R.string.calcular_gorjeta),
@@ -70,10 +90,23 @@ fun CalculadorApp(modifier: Modifier = Modifier
         CampoNumericoEditavel(
             modifier.fillMaxWidth(),
             value = valorContaTexto,
-            onValueChange = {valorContaTexto = it}
+            onValueChange = {valorContaTexto = it},
+            label = stringResource(R.string.valor_conta),
+            leadingIcon = R.drawable.baseline_attach_money_24
+        )
+        CampoNumericoEditavel(
+            modifier.fillMaxWidth(),
+            value = porcentagemGorjeta,
+            onValueChange = {porcentagemGorjeta = it},
+            label = stringResource(R.string.porcentagem_da_gorjeta),
+            leadingIcon = R.drawable.baseline_percent_24
+        )
+        ChaveArrendondarGorjeta(
+            checked = estadoBotao,
+            onCheckedChange = {estadoBotao = it}
         )
 
-        Spacer(modifier = Modifier.height(32.dp))
+        Spacer(modifier = Modifier.height(25.dp))
         Text(
             stringResource(R.string.valor_gorjeta, gorjeta),
             style = MaterialTheme.typography.headlineSmall,
@@ -84,18 +117,53 @@ fun CalculadorApp(modifier: Modifier = Modifier
     }
 }
 @Composable
-fun CampoNumericoEditavel(modifier: Modifier = Modifier, value: String ,onValueChange: (String) -> Unit){
+fun CampoNumericoEditavel(
+    modifier: Modifier = Modifier,
+    value: String, onValueChange: (String) -> Unit,
+    label: String = "",
+    leadingIcon: Int = 0,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    ){
 
     TextField(value = value,
         onValueChange = onValueChange,
-        label = {Text(stringResource(R.string.valor_conta))},
-        modifier = Modifier.fillMaxWidth(),
+        label = {Text(label)},
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(bottom = 20.dp),
         singleLine = true,
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        leadingIcon = { Icon(painterResource(id = leadingIcon), "")},
+        keyboardOptions = keyboardOptions
     )
 }
 
-private fun calcularGorjeta(quantia: Double, porcentagemGorjeta: Double = 15.0): String {
-    val gorjeta = porcentagemGorjeta / 100 * quantia
+@Composable
+fun ChaveArrendondarGorjeta(
+    modifier: Modifier = Modifier,
+    checked: (Boolean) = false,
+    onCheckedChange: (Boolean) -> Unit = {}
+){
+    Row (
+        modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Text(
+            stringResource(R.string.arrendondar_gorjeta)
+        )
+        Switch(
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            modifier = modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.End)
+        )
+    }
+}
+
+private fun calcularGorjeta(quantia: Double, porcentagemGorjeta: Double, estadoBotao: Boolean): String {
+    var gorjeta = porcentagemGorjeta / 100 * quantia
+    if (estadoBotao){
+        gorjeta = kotlin.math.ceil(gorjeta)
+    }
     return NumberFormat.getCurrencyInstance().format(gorjeta)
 }
